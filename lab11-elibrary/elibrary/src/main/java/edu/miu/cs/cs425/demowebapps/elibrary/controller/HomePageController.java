f01ea90bc60568063334761332a108ccb690cbfd
Serkalem <serkalembekele081@gmail.com>
2022-01-27 00:56:39 -0600

    finish lab11 part1

---
+++ lab11-elibrary/elibrary/src/main/java/edu/miu/cs/cs425/demowebapps/elibrary/controller/HomePageController.java
@@ -0,0 +1,14 @@
+package edu.miu.cs.cs425.demowebapps.elibrary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomePageController {
+	@GetMapping(value={"/","/elibrary","/elibrary/home"})
+	public String displayHomepage() {
+		return "home/index";
+		
+	}
+
+}
